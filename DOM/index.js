8cc711c8038945569b66dea416df443e3b5aa5a5
davecranenuke <dave.crane@pyrolico.com>
2022-07-24 20:02:09 -0700

    commit Dom Manipulation

---
+++ DOM/index.js
@@ -0,0 +1,4 @@
+// document.querySelector("h1").innerHTML = "Good Bye";
+document.querySelector("h1").classList.add("huge");
+document.querySelector("h1").textContent = "Good Bye";
+document.querySelector("h1").innerHTML = "<em>Good Bye<em>"
