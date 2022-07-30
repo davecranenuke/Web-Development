ffe1fee2538f2e152fd93512276f09d3a2a45fcc
davecranenuke <dave.crane@pyrolico.com>
2022-07-30 03:19:52 -0700

    drum kit update

--- drumKit/Drum Kit Starting Files/Drum Kit Starting Files/index.js
+++ drumKit/Drum Kit Starting Files/Drum Kit Starting Files/index.js
@@ -2,16 +2,60 @@
 
 for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
   document.querySelectorAll(".drum")[i].addEventListener("click", handleClick);
-
+  document.querySelectorAll(".drum")[i].addEventListener("keypress", handleClick);
 }
 
 // var audio = new Audio('audio_file.mp3');
 // audio.play();
 
 function handleClick() {
-  buttonPressed = this;
-  buttonPressed.classList.add("pressed")
-  var audio = new Audio('sounds/tom-1.mp3');
-  audio.play();
-  setTimeout(() => {buttonPressed.classList.remove("pressed");}, 50);
+
+  if(event.type == "keypress") {
+    var buttonInnerHTML = event.key;
+  } else {
+    var buttonPressed = this;
+    buttonPressed.classList.add("pressed")
+    setTimeout(() => {buttonPressed.classList.remove("pressed");}, 50);
+    var buttonInnerHTML = this.innerHTML;
+  }
+
+  console.log(event); // all event related info
+  console.log(event.type);
+  console.log(event.key);
+  console.log(event.code);
+
+  console.log("click value", buttonInnerHTML);
+
+  switch(buttonInnerHTML) {
+    case "w":
+      var audio = new Audio('sounds/tom-1.mp3');
+      audio.play();
+    break;
+    case "a":
+      var audio = new Audio('sounds/tom-2.mp3');
+      audio.play();
+    break;
+    case "s":
+      var audio = new Audio('sounds/tom-3.mp3');
+      audio.play();
+    break;
+    case "d":
+      var audio = new Audio('sounds/tom-4.mp3');
+      audio.play();
+    break;
+    case "j":
+      var audio = new Audio('sounds/snare.mp3');
+      audio.play();
+    break;
+    case "k":
+      var audio = new Audio('sounds/kick-bass.mp3');
+      audio.play();
+    break;
+    case "l":
+      var audio = new Audio('sounds/crash.mp3');
+      audio.play();
+    break;
+    default:
+      console.log("Wrong key pressed");
+  }
 }
